ab22c924e43e246faa5124aaa83b99a6b6eda568
OldBrain <hel_brain@mail.ru>
2022-05-30 01:41:09 +0400

    Добавлен TimeDialog

--- app/src/main/java/ru/avbugorov/tcalc/ui/home/HomeFragment.java
+++ app/src/main/java/ru/avbugorov/tcalc/ui/home/HomeFragment.java
@@ -1,19 +1,30 @@
 package ru.avbugorov.tcalc.ui.home;
 
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
+import android.arch.lifecycle.ViewModelProvider;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
-import android.arch.lifecycle.ViewModelProvider;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 import ru.avbugorov.tcalc.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private TimePickerDialog timePickerDialog;
+    EditText editTextTimeBegin;
+    EditText editTextTimeEnd;
+    final Calendar myCalender = Calendar.getInstance();
+    int hour = myCalender.get(Calendar.HOUR_OF_DAY);
+    int minute = myCalender.get(Calendar.MINUTE);
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,9 +34,37 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        editTextTimeBegin = binding.editTextTimeBegin;
+        editTextTimeEnd = binding.editTextTimeEnd;
+
+        showTimeDialog(editTextTimeBegin);
+        showTimeDialog(editTextTimeEnd);
+
+
 //        final TextView textView = binding.textHome;
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    private void showTimeDialog(EditText editText) {
+        editText.setText(hour + ":" + minute);
+        editText.setShowSoftInputOnFocus(false);
+
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePickerDialog = new TimePickerDialog(HomeFragment.this.getContext(), AlertDialog.THEME_HOLO_LIGHT, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        editText.setText(hourOfDay + ":" + minute);
+
+                    }
+                }, hour, minute, true);
+
+
+                timePickerDialog.show();
+            }
+        });
     }
 
     @Override
